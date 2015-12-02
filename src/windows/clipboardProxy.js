924cded03e0e748ef00a6f7eb7ae9dc8de28d910
janpeer <janpeerharries@aol.com>
2015-12-02 10:54:03 +0100

    add paste function

--- src/windows/clipboardProxy.js
+++ src/windows/clipboardProxy.js
@@ -1,15 +1,15 @@
-var cordova = require('cordova'),
-    channel = require('cordova/channel');
-
 module.exports = {
     copy: function (success, fail, args) {
-        console.log("COPY");
         var value = new Windows.ApplicationModel.DataTransfer.DataPackage();
         value.setText(args[0]);
         Windows.ApplicationModel.DataTransfer.Clipboard.setContent(value);
+        success(true);
     },
     paste: function (success, fail, args) {
-        console.log("PASTE");
+        var dataPackageView = Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
+        dataPackageView.getTextAsync().done(function completed(result) {
+            success(true);
+        });
     }
 }
 require("cordova/exec/proxy").add("Clipboard", module.exports);
